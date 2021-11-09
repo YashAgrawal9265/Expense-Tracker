8fdfcb6fa263256beee872cd7a34f0242f9cbc78
yash9265 <yashagrawal9265@gmail.com>
2021-11-10 00:35:26 +0530

    Added ExpenseDate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,15 @@
-import './ExpenseItem.css'
-
-function ExpenseItem() {
-    const expenseDate = new Date();
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 234;
-    return (
-        <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
-            <div className="expense-item__description ">
-                <h1>{expenseTitle}</h1>
-                <div className="expense-item__price">${expenseAmount}</div>
-            </div>
-        </div>
-    );
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description ">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
